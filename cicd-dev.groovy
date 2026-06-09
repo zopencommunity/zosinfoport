40dd29d7696b9030a54e73f7e8d228327ba96c4d
John Parke <john.parke@alebra.com>
2026-06-09 05:35:22 -0500

    updated cicd-dev.groovy

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -2,11 +2,11 @@ node('linux') {
   stage ('Poll') {
     checkout([
       $class: 'GitSCM', branches: [[name: '*/main']], extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/zoOSInfoport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/zosinfoport.git']]])
   }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [
-      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/zoOSInfoport.git'),
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/zosinfoport.git'),
       string(name: 'PORT_DESCRIPTION', value: 'zOSInfo is a lightweight C++ utility for retrieving system-level information from z/OS Unix System Services (USS).'),
       string(name: 'BUILD_LINE', value: 'DEV')
     ]
